c023b3cea0698660c89ce268bb4c0d658d7d19ea
Steven Skoczen <steven.skoczen@wk.com>
2012-09-13 15:48:18 -0700

    CORS support in

--- project/apps/archetype/management/commands/create_bucket.py
+++ project/apps/archetype/management/commands/create_bucket.py
@@ -1,9 +1,17 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from boto.s3.connection import S3Connection
+from boto.s3.cors import CORSConfiguration
+
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         c = S3Connection(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
-        c.create_bucket(settings.AWS_STORAGE_BUCKET_NAME)
+        bucket = c.lookup(settings.AWS_STORAGE_BUCKET_NAME)
+        if not bucket:
+            bucket = c.create_bucket(settings.AWS_STORAGE_BUCKET_NAME)
+
+        cors_cfg = CORSConfiguration()
+        cors_cfg.add_rule('GET', '*')
+        bucket.set_cors(cors_cfg)
